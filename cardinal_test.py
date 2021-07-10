39723d4c059d6e1087252cfd7eef99268297925a
vietcheems <731pkmn@gmail.com>
2021-07-10 13:09:45 +0700

    handled up to 999 999 999 999

--- cardinal_test.py
+++ cardinal_test.py
@@ -1,20 +1,24 @@
 import unittest
 import pynini
 from nemo_text_processing.inverse_text_normalization.taggers.cardinal import CardinalFst
-from pynini.lib.rewrite import rewrites
+from pynini.lib.rewrite import top_rewrite
 class TestCardinalFst(unittest.TestCase):
     graph = CardinalFst().fst
     
     def test_2_digits(self):
         cardinal = CardinalFst()
         word_list = [
-            "mười ",
+            "âm hai",
             "mười hai",
             "mười lăm",
             "hai mươi",
             "bốn mươi lăm",
             "chín bảy",
-            "ba mốt"
+            "âm ba mốt",
+            "hai năm"
         ]
         for word in word_list:
-            print(word, rewrites(word, self.graph))
+            print(word + " | " + top_rewrite(word, self.graph))
+
+if __name__ == "__main__":
+    TestCardinalFst().test_2_digits()
--- nemo_text_processing/inverse_text_normalization/taggers/cardinal.py
+++ nemo_text_processing/inverse_text_normalization/taggers/cardinal.py
@@ -51,20 +51,20 @@ class CardinalFst(GraphFst):
         graph_million_end = pynutil.delete("triệu")
         graph_billion_end = pynini.union(pynutil.delete("tỉ"), pynutil.delete("tỷ"))
 
-        graph_ten = pynini.string_file(get_abs_path("data/numbers/ten.tsv")) + pynini.union(graph_digit_any, pynutil.insert("0"))
+        graph_ten = pynini.string_file(get_abs_path("data/numbers/ten.tsv")) + pynini.union(delete_space + graph_digit_any, pynutil.insert("0"))
 
         graph_2_9_muoi = graph_digit_non_zero + delete_space + pynini.union(
             pynutil.delete("mươi") + pynini.union(delete_space + graph_digit_any, pynutil.insert("0")),
             graph_digit_any_non_zero)
 
-
         graph_hundred_component = pynini.union(graph_digit + delete_space + graph_hundred_end, pynutil.insert("0"))
-        # graph_hundred_component += delete_space
+        graph_hundred_component += pynini.closure(delete_space, 0, 1) 
         graph_hundred_component += pynini.union(
-            delete_space
-            + pynini.union(graph_ten, graph_2_9_muoi), pynutil.insert("00")
-            )
-
+            graph_ten | graph_2_9_muoi,
+            pynini.union(pynutil.delete("linh"), 
+            pynutil.delete("lẻ")) + pynutil.insert("0") + delete_space + graph_digit_any | pynutil.insert("00"))
+        graph_hundred_component = pynini.union(graph_hundred_component, pynutil.insert("00") + graph_digit)
+        
         graph_hundred_component_at_least_one_none_zero_digit = graph_hundred_component @ (
             pynini.closure(NEMO_DIGIT) + (NEMO_DIGIT - "0") + pynini.closure(NEMO_DIGIT)
         )
@@ -73,45 +73,45 @@ class CardinalFst(GraphFst):
         )
 
         graph_thousands = pynini.union(
-            graph_hundred_component_at_least_one_none_zero_digit + delete_space + graph_thousands_end,
+            graph_hundred_component + delete_space + graph_thousands_end,
             pynutil.insert("000", weight=0.1),
         )
 
         graph_million = pynini.union(
-            graph_hundred_component_at_least_one_none_zero_digit + delete_space + graph_million_end,
+            graph_hundred_component + delete_space + graph_million_end,
             pynutil.insert("000", weight=0.1),
         )
 
         graph_billion = pynini.union(
-            graph_hundred_component_at_least_one_none_zero_digit + delete_space + graph_billion_end,
+            graph_hundred_component + delete_space + graph_billion_end,
             pynutil.insert("000", weight=0.1),
         )
-        graph_trillion = pynini.union(
-            graph_hundred_component_at_least_one_none_zero_digit + delete_space + pynutil.delete("trillion"),
-            pynutil.insert("000", weight=0.1),
-        )
-        graph_quadrillion = pynini.union(
-            graph_hundred_component_at_least_one_none_zero_digit + delete_space + pynutil.delete("quadrillion"),
-            pynutil.insert("000", weight=0.1),
-        )
-        graph_quintillion = pynini.union(
-            graph_hundred_component_at_least_one_none_zero_digit + delete_space + pynutil.delete("quintillion"),
-            pynutil.insert("000", weight=0.1),
-        )
-        graph_sextillion = pynini.union(
-            graph_hundred_component_at_least_one_none_zero_digit + delete_space + pynutil.delete("sextillion"),
-            pynutil.insert("000", weight=0.1),
-        )
+        # graph_trillion = pynini.union(
+        #     graph_hundred_component_at_least_one_none_zero_digit + delete_space + pynutil.delete("trillion"),
+        #     pynutil.insert("000", weight=0.1),
+        # )
+        # graph_quadrillion = pynini.union(
+        #     graph_hundred_component_at_least_one_none_zero_digit + delete_space + pynutil.delete("quadrillion"),
+        #     pynutil.insert("000", weight=0.1),
+        # )
+        # graph_quintillion = pynini.union(
+        #     graph_hundred_component_at_least_one_none_zero_digit + delete_space + pynutil.delete("quintillion"),
+        #     pynutil.insert("000", weight=0.1),
+        # )
+        # graph_sextillion = pynini.union(
+        #     graph_hundred_component_at_least_one_none_zero_digit + delete_space + pynutil.delete("sextillion"),
+        #     pynutil.insert("000", weight=0.1),
+        # )
 
-        graph = graph_sextillion 
-        graph += delete_space
-        graph += graph_quintillion
-        graph += delete_space
-        graph += graph_quadrillion
-        graph += delete_space
-        graph += graph_trillion
-        graph += delete_space
-        graph += graph_billion
+        # graph = graph_sextillion 
+        # graph += delete_space
+        # graph += graph_quintillion
+        # graph += delete_space
+        # graph += graph_quadrillion
+        # graph += delete_space
+        # graph += graph_trillion
+        # graph += delete_space
+        graph = graph_billion
         graph += delete_space
         graph += graph_million
         graph += delete_space
@@ -120,23 +120,25 @@ class CardinalFst(GraphFst):
         graph += graph_hundred_component
 
         graph = graph @ pynini.union(
-            pynutil.delete(pynini.closure("0")) + pynini.difference(NEMO_DIGIT, "0") + pynini.closure(NEMO_DIGIT), "0"
+            pynutil.delete(pynini.closure("0")) + pynini.difference(NEMO_DIGIT, "0") + pynini.closure(NEMO_DIGIT), 
+            pynutil.delete(pynini.closure("0")) + pynutil.insert("0")
         )
 
-        labels_exception = [num_to_word(x) for x in range(0, 10)]
-        graph_exception = pynini.union(*labels_exception)
+
 
         # graph = pynini.cdrewrite(pynutil.delete("and"), NEMO_SPACE, NEMO_SPACE, NEMO_SIGMA) @ graph
 
         self.graph_no_exception = graph
 
-        self.graph = (pynini.project(graph, "input") - graph_exception.arcsort()) @ graph
 
         optional_minus_graph = pynini.closure(
-            pynutil.insert("negative: ") + pynini.union(pynini.cross("âm", "\"-\""), pynini.cross("âm", "\"-\"")) + NEMO_SPACE, 0, 1
+            pynutil.insert("negative: ") + pynini.union(pynini.cross("âm", "\"-\""), pynini.cross("trừ", "\"-\"")) + NEMO_SPACE, 0, 1
         )
+        graph = optional_minus_graph + pynutil.insert("integer: \"") + graph + pynutil.insert("\"")
 
-        final_graph = optional_minus_graph + pynutil.insert("integer: \"") + self.graph + pynutil.insert("\"")
+        labels_exception = ["không", "một", "hai", "ba", "bốn", "năm", "sáu", "bảy", "bẩy", "tám", "chín"]
+        graph_exception = pynini.union(*labels_exception)
+        graph = (pynini.project(graph, "input") - graph_exception.arcsort()) @ graph
 
-        final_graph = self.add_tokens(final_graph)
+        final_graph = self.add_tokens(graph)
         self.fst = final_graph.optimize()
