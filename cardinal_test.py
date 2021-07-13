38a4d442d9f763329a5de2f8e329c14ef9a5e8b2
vietcheems <731pkmn@gmail.com>
2021-07-13 21:08:26 +0700

    added space every 3 digits: 1000 -> 1 000

--- cardinal_test.py
+++ cardinal_test.py
@@ -4,6 +4,7 @@ from nemo_text_processing.inverse_text_normalization.taggers.cardinal import Car
 from pynini.lib.rewrite import top_rewrite
 class TestCardinalFst(unittest.TestCase):
     graph = CardinalFst().fst
+    leading_zero = CardinalFst().remove_leading_zero_graph
     
     def test_2_digits(self):
         cardinal = CardinalFst()
@@ -11,13 +12,13 @@ class TestCardinalFst(unittest.TestCase):
             "âm hai",
             "bốn lăm",
             "trừ một tỷ chín triệu sáu trăm nghìn ba trăm hai mốt",
-            "trừ một tỷ chín triệu sáu trăm nghìn ba trăm hai mốt",
-            "một nghìn tỷ",
-            "một nghìn tỷ tỷ",
-            "chín trăm chín chín nghìn tỷ tỷ"
+            "một trăm",
+            "chín trăm chín chín nghìn tỷ tỷ",
+            # "000 100 000"
         ]
         for word in word_list:
             print(word + " | " + top_rewrite(word, self.graph))
+            # print(top_rewrite(word, self.leading_zero))
 
 if __name__ == "__main__":
     TestCardinalFst().test_2_digits()
--- draft.py
+++ draft.py
@@ -2,4 +2,4 @@ import pynini
 import sys
 from nemo_text_processing.inverse_text_normalization.inverse_normalize import InverseNormalizer
 
-print(InverseNormalizer().inverse_normalize("hai hai hai", verbose=True))
+print(InverseNormalizer().inverse_normalize("hai hai hai blah một tỷ blah blah hai phần ba blah blo hai nghìn phẩy tám", verbose=True))
