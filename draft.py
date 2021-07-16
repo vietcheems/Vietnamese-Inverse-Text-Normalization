0afea268af2a82f98bf9c28ebc8d6d2b35e0bf75
vietcheems <731pkmn@gmail.com>
2021-07-16 23:07:19 +0700

    time.py created

--- draft.py
+++ draft.py
@@ -2,4 +2,4 @@ import pynini
 import sys
 from nemo_text_processing.inverse_text_normalization.inverse_normalize import InverseNormalizer
 
-print(InverseNormalizer().inverse_normalize("", verbose=True))
+print(InverseNormalizer().inverse_normalize("ngày mùng một tháng hai năm một", verbose=True))
