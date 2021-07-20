48d77e324bae528fc1546408fd7f5f31545d07c4
vietcheems <731pkmn@gmail.com>
2021-07-20 23:32:16 +0700

    added exporting tool and far folder

--- draft.py
+++ draft.py
@@ -1,5 +1,8 @@
 import pynini
+import pywrapfst
 import sys
+import time
+from pynini.lib.rewrite import rewrites
 from nemo_text_processing.inverse_text_normalization.inverse_normalize import InverseNormalizer
 
-print(InverseNormalizer().inverse_normalize("hai mươi giờ kém hai mươi ngày mùng một tháng hai năm một", verbose=True))
+print(InverseNormalizer().inverse_normalize("hai rưỡi ngày mùng một tháng hai năm hai không hai mươi", verbose=True))
